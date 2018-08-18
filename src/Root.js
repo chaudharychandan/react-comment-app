c2f06f863b9fa6683b5f739d4869872537046c59
Chandan Kumar <chandanchaudhary@outlook.com>
2018-08-18 23:33:04 +0530

    Add react-thunk middleware

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
 import reducers from 'reducers';
 
 export default ({children, initialState = {}}) => {
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(thunk)
+  );
+
   return (
-    <Provider store={createStore(reducers, initialState)}>
+    <Provider store={store}>
       {children}
     </Provider>
   );
