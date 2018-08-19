c23091b00092c8ecf0cd436491030e1093fc22c9
Chandan Kumar <chandanchaudhary@outlook.com>
2018-08-19 13:05:56 +0530

    Navigate to home route if user tries to go to post route if not loggedin

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -5,6 +5,20 @@ import * as actions from 'actions';
 class CommentBox extends Component {
   state = { comment: '' };
 
+  componentDidMount() {
+    this.shouldNavigateAway();
+  }
+
+  componentDidUpdate() {
+    this.shouldNavigateAway();
+  }
+
+  shouldNavigateAway = () => {
+    if (!this.props.auth) {
+      this.props.history.push('/');
+    }
+  }
+
   handleChange = event => {
     this.setState({ comment: event.target.value });
   }
@@ -31,4 +45,8 @@ class CommentBox extends Component {
   }
 }
 
-export default connect(null, actions)(CommentBox);
+const mapStateToProps = ({ auth }) => ({
+  auth
+});
+
+export default connect(mapStateToProps, actions)(CommentBox);
